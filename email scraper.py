0b263534af1b217de6c546c71f3ab6b32496ceee
Harsh Gupta <hg242322@gmail.com>
2021-08-21 15:06:21 +0530

    v1.0

--- email scraper.py
+++ email scraper.py
@@ -7,19 +7,34 @@ from pymongo import MongoClient
 from pyfiglet import figlet_format
 from rich.console import Console
 from rich.markdown import Markdown
+import json
+from dotenv import load_dotenv
+import os
 
 
-email_regex = r'[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+'
+load_dotenv()
+try:
+    python_env = os.environ['python_env']
+    mongodb_url = os.environ['mongodb_url']
+    root_url = os.environ['root_url']
+    if not root_url.endswith('/'):
+        root_url += '/'
+    email_regex = os.environ['email_regex']
+    user_agent = os.environ['user_agent']
+except Exception as e:
+    print('\nImproperly Configured Environment\nRefer the documentation for Email Scraper (IEEE-VIT)\nhttps://github.com/IEEE-VIT/email-scrapper\n')
+    print(e)
+    exit(0)
+
+
 page_keywords = ['about', 'contact', 'support']
-
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
 headers = {'User-Agent': user_agent}
 
 
-connection = MongoClient('mongodb://localhost:27017/')
+connection = MongoClient(mongodb_url)
 db = connection.get_database('sponsorship')
 companies = db['companies']
-companies_skip = db['companies_skip']
+skipped = db['skipped']
 
 
 print(figlet_format('Email  Scraper'))
@@ -30,6 +45,8 @@ console = Console()
 console.print(Markdown('##### Welcome to my command line utility'))
 console.print(Markdown('###### You may enter 0 OR skip to skip any company | -1 OR exit to quit the command line utility'))
 
+
+mode = ''
 while True:
     mode = input('\nPlease select any mode to continue\nauto OR manual\n').lower()
     if mode == 'auto' or mode == 'manual':
@@ -37,24 +54,47 @@ while True:
 
 
 def main():
-    internshala = 'https://internshala.com/internships/'
-    pages = int(BeautifulSoup(requests.get(internshala).content, 'lxml').find(id='total_pages').get_text())
-    # for page in range(1, pages+1):
-    for page in range(1, 2):
-        company_names = getCompanyNames(f'{internshala}page-{page}')
+    global mode
+    pages = int(BeautifulSoup(requests.get(root_url).content, 'lxml').find(id='total_pages').get_text()) if python_env == 'production' else 1
+    for page in range(1, pages+1):
+        company_names = getCompanyNames(f'{root_url}page-{page}')
         for company_name in company_names:
             company_name = company_name.strip()
-            if companies.find_one({"name": company_name}) or companies_skip.find_one({"name": company_name}):
+            if companies.find_one({"name": company_name}) or skipped.find_one({"name": company_name}):
                 continue
             print(f'\n\n{company_name}')
             info = findInfo(company_name)
             if info == 0:
-                companies_skip.insert_one({"name": company_name, "source": "Internshala"})
+                print('skipped :(')
+                skipped.insert_one({"name": company_name, "source": "Internshala", "mode": mode})
                 continue
             else:
                 info = {"name": company_name, "website": info['website'], "emails": info['emails'], "source": "Internshala"}
                 print(info)
                 companies.insert_one(info)
+
+    view = input('\n\nWould you like to view automatically skipped companies\n').lower()
+    if not (view == 'y' or view == 'yes'):
+        console.print(Markdown('##### Thank You !! for using my Email Scraper command line utility | Harsh Gupta (IEEE-VIT)'))
+        print()
+        exit(0)
+
+    mode = 'manual'
+    console.print(Markdown('##### Switching to manual mode'))
+
+    for company in skipped.find({"mode": "auto"}):
+        print(f'\n\n{company["name"]}')
+        skipped.delete_one(company)
+        info = findInfo(company['name'])
+        if info == 0:
+            print('skipped :(')
+            skipped.insert_one({"name": company_name, "source": "Internshala", "mode": mode})
+            continue
+        else:
+            info = {"name": company_name, "website": info['website'], "emails": info['emails'], "source": "Internshala"}
+            print(info)
+            companies.insert_one(info)
+
 
 
 def getCompanyNames(url):
@@ -111,27 +151,48 @@ def filterPages(link):
     return False
 
 
+def logJson(filename, json_obj):
+    f = open(filename, 'a+')
+    f.seek(0)
+    if f.read():
+        f.seek(0)
+        temp = json.load(f)
+        temp.append(json_obj)
+        f.truncate(0)
+        json.dump(temp, f, indent=2)
+        f.close()
+    else:
+        json.dump([json_obj], f, indent=2)
+        f.close()
+
+
 def findEmails(url):
     info = {"website": url}
-    emails = scrapeEmails(url)
-    if emails:
-        info['emails'] = list(set(emails))
-        return info
-    links = BeautifulSoup(requests.get(url, headers=headers).content, 'lxml').find_all('a')
-    links = [str(link.get('href')) for link in links]
+    emails = set()
+    emails.update(scrapeEmails(url))
+    
+    res = requests.get(url, headers=headers)
+    logJson('response.json', {"url": url, "response": str(res)})
+    links = BeautifulSoup(res.content, 'lxml').find_all('a')
+    links = [str(link.get('href')) for link in links if 'href' in link.attrs]
     links = list(filter(filterPages, links))
-    emails = []
+
     for link in links:
-        if not (link.startswith('http://') or link.startswith('https://')):
-            link = urljoin(url, link, allow_fragments=True)
-        emails.extend(scrapeEmails(link))
-    info['emails'] = list(set(emails))
+        try:
+            if not (link.startswith('http://') or link.startswith('https://')):
+                link = urljoin(url, link, allow_fragments=True)
+            emails.update(scrapeEmails(link))
+        except Exception as e:
+            logJson('errors.json', {"url": link, "exception": str(e)})
+    
+    info['emails'] = list(emails)
     return info
     
 
 def scrapeEmails(url):
-    res = requests.get(url, headers=headers).content
-    soup = BeautifulSoup(res, 'lxml').get_text()
+    res = requests.get(url, headers=headers)
+    logJson('response.json', {"url": url, "response": str(res)})   
+    soup = BeautifulSoup(res.content, 'lxml').get_text()
     return re.findall(email_regex, soup)
 
 
